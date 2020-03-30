550508be059ef8fb7c43b6b441adcac4d87e0f97
Amanda Hogan <ahogan@teamworks.com>
2020-03-30 15:36:07 -0400

    BeerReducer export

--- tappr-fe/src/reducers/beerReducer.js
+++ tappr-fe/src/reducers/beerReducer.js
@@ -1,5 +1,12 @@
-export const initialState = {
+const initialState = {
     beer: [],
     isFetching: false,
     error: ''
-}
+};
+
+export default beerReducer = (state = initialState, action) => {
+  switch(action.type) {
+    default:
+      return state
+  };
+};
