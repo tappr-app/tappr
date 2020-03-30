99b8091b8a5466b6c3db55bdbacda6c4ccd5f6dd
Landon Turner <lturner@LTurner-MBP.lan1>
2020-03-30 12:57:23 -0400

    adds some verification to the form

--- tappr-fe/src/components/RegisterForm.js
+++ tappr-fe/src/components/RegisterForm.js
@@ -8,13 +8,17 @@ const RegisterForm = props =>{
     return(
         <FormDiv>
             <label>Username</label>
-            <input name='username'></input>
+            <input name='username' ref={register({required: true, maxLength: 20})}></input>
 
             <label>Password</label>
-            <input type='password' name='password'></input>
-            {/* add a second Password input for verification? Possible stretch? */}
-            <label>Birthday</label>
-            <input type='date' name='birthday'></input>
+            <input type='password' name='password' ref={register({required: true, minLength: 8})}></input>
+            {/* add a second Password input for verification? Possible stretch? Also make some regex requirements? */}
+            <label>Age</label>
+            <input type='number' name='age' ref={register({required: true, min: 21, max: 99})}></input>
+
+            {/* <label>Birthday</label>
+            <input type='date' name='birthday' ref={register({required})}></input> 
+            Make a birthday format? Write function to verify user age (is this an action or should I write the fn in the scope of this component?) */}
 
             <label>Bio(Optional)</label>
             <textarea></textarea>
