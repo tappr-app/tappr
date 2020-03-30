a4f224576dfd1339d6b20591a4d7221a91994f27
Landon Turner <lturner@LTurner-MBP.lan1>
2020-03-30 13:53:00 -0400

    validation is coming along, still wip

--- tappr-fe/src/components/RegisterForm.js
+++ tappr-fe/src/components/RegisterForm.js
@@ -4,27 +4,36 @@ import { connect } from 'react-redux'
 import { FormDiv } from '../styles/Styled'
 
 const RegisterForm = props =>{
-    const { register, handleSubmit, watch, errors } = useForm();
+    const { register, handleSubmit, watch, errors } = useForm({
+        validateCriteriaMode: "all"
+      });
+    const onSubmit = data => console.log(data)
     return(
         <FormDiv>
-            <label>Username</label>
-            <input name='username' ref={register({required: true, maxLength: 10})}></input>
-            {errors.username &&
-                errors.username.type === 'required' && 'Required Field'}
-            {errors.username &&
-                errors.username.type === 'maxLength' && 'Please use less than 10 characters'}
-            <label>Password</label>
-            <input type='password' name='password' ref={register({required: true, minLength: 8})}></input>
-            {/* add a second Password input for verification? Possible stretch? Also make some regex requirements? */}
-            <label>Age</label>
-            <input type='number' name='age' ref={register({required: true, min: 21, max: 99})}></input>
+                <form className='register-user' onSubmit={handleSubmit(onSubmit)}>
+                <label>Username</label>
+                <input name='username'
+                 ref={register({ required: true, maxLength: 10})}/>              
+                {errors.username &&
+                    errors.username.type === 'required' && 'Required Field'}
+                {errors.username &&
+                    errors.username.type === 'maxLength' && 'Please use less than 10 characters'}
+                <label>Password</label>
+                <input type='password' name='password' ref={register({required: true, minLength: 8})}></input>
+                {/* add a second Password input for verification? Possible stretch? Also make some regex requirements? */}
+                <label>Age</label>
+                <input type='number' name='age' ref={register({required: true, min: 21, max: 99})}></input>
 
-            {/* <label>Birthday</label>
-            <input type='date' name='birthday' ref={register({required})}></input> 
-            Make a birthday format? Write function to verify user age (is this an action or should I write the fn in the scope of this component?) */}
+                {/* <label>Birthday</label>
+                <input type='date' name='birthday' ref={register({required})}></input> 
+                Make a birthday format? Write function to verify user age (is this an action or should I write the fn in the scope of this component?) */}
 
-            <label>Bio(Optional)</label>
-            <textarea></textarea>
+                <label>Bio(Optional)</label>
+                <textarea></textarea>
+                <button type='submit'>Register</button>
+                <label>Already registered?</label>
+                <button>Login</button>
+            </form>
         </FormDiv>
     )
 }
