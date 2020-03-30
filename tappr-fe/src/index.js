9e1b62c43baae1a1e553e445db5e213aab5f87b2
Landon Turner <lturner@LTurner-MBP.lan1>
2020-03-30 11:27:37 -0400

    set up store in redux

--- tappr-fe/src/index.js
+++ tappr-fe/src/index.js
@@ -1,15 +1,37 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router } from 'react-router-dom'
+
+import { createStore, applyMiddleWare} from 'redux';
+import { beerReducer } from './reducers/beerReducer'
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
+import logger from 'redux-logger';
+
+
+
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+
+const store = createStore(beerReducer, applyMiddleWare(thunk, logger))
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <Router>
+        <App /> 
+      </Router>
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
+
+// IF IT BREAKS USE STRICT MODE vvv
+{/* <React.StrictMode>
+<App />
+</React.StrictMode> */}
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
---
+++ tappr-fe/src/reducers/beerReducer.js
@@ -0,0 +1,5 @@
+export const initialState = {
+    beer: [],
+    isFetching: false,
+    error: ''
+}
