eeab3dffb9824ad4a13f5fbee5beb755ddfe7011
Landon Turner <lturner@LTurner-MBP.lan1>
2020-03-30 14:13:16 -0400

    forms structured, some validation exists through useForm. can add yup later

--- tappr-fe/src/components/RegisterForm.js
+++ tappr-fe/src/components/RegisterForm.js
@@ -8,39 +8,41 @@ const RegisterForm = props =>{
 
     const onSubmit = data => console.log(data)
     return(
+        
         <FormDiv>
-                <form className='register-user' onSubmit={handleSubmit(onSubmit)}>
-                <label>Username</label>
-                <input name='username'
-                 ref={register({ required: true, maxLength: 10})}/>              
-                {errors.username &&
-                    errors.username.type === 'required' && 'Required Field'}
-                {errors.username &&
-                    errors.username.type === 'maxLength' && 'Please use less than 10 characters'}
-                <label>Password</label>
-                <input type='password' name='password' ref={register({required: true, minLength: 8})}></input>
-                {errors.password &&
-                    errors.password.type === 'required' && 'Required Field'}
-                {errors.username &&
-                    errors.password.type === 'minLength' && 'Please use at least 8 characters'}
-                {/* add a second Password input for verification? Possible stretch? Also make some regex requirements? */}
-                <label>Age</label>
-                <input type='number' name='age' ref={register({required: true, min: 21})}></input>
-                {errors.username &&
-                    errors.age.type === 'required' && 'Required Field'}
-                {errors.username &&
-                    errors.age.type === 'min' && 'Must be at least 21!'}
+            <h2 className='form-title'>Register</h2>
+            <form className='register-user' onSubmit={handleSubmit(onSubmit)}>
+            <label>Username</label>
+            <input name='username'
+                ref={register({ required: true, maxLength: 10})}/>              
+            {errors.username &&
+                errors.username.type === 'required' && 'Required Field'}
+            {errors.username &&
+                errors.username.type === 'maxLength' && 'Please use less than 10 characters'}
+            <label>Password</label>
+            <input type='password' name='password' ref={register({required: true, minLength: 8})}></input>
+            {errors.password &&
+                errors.password.type === 'required' && 'Required Field'}
+            {errors.username &&
+                errors.password.type === 'minLength' && 'Please use at least 8 characters'}
+            {/* add a second Password input for verification? Possible stretch? Also make some regex requirements? */}
+            <label>Age</label>
+            <input type='number' name='age' ref={register({required: true, min: 21})}></input>
+            {errors.username &&
+                errors.age.type === 'required' && 'Required Field'}
+            {errors.username &&
+                errors.age.type === 'min' && 'Must be at least 21!'}
 
-                {/* <label>Birthday</label>
-                <input type='date' name='birthday' ref={register({required})}></input> 
-                Make a birthday format? Write function to verify user age (is this an action or should I write the fn in the scope of this component?) */}
+            {/* <label>Birthday</label>
+            <input type='date' name='birthday' ref={register({required})}></input> 
+            Make a birthday format? Write function to verify user age (is this an action or should I write the fn in the scope of this component?) */}
 
-                <label>Bio(Optional)</label>
-                <textarea name='bio' ref={register({ maxLength: 500 })}></textarea>
-                <button type='submit'>Register</button>
-                <label>Already registered?</label>
-                <button>Login</button>
-            </form>
+            <label>Bio(Optional)</label>
+            <textarea name='bio' ref={register({ maxLength: 500 })}></textarea>
+            <button type='submit'>Register</button>
+            <label>Already registered?</label>
+            <button>Login</button>
+        </form>
         </FormDiv>
     )
 }
--- tappr-fe/src/styles/Styled.js
+++ tappr-fe/src/styles/Styled.js
@@ -1,10 +1,10 @@
 import styled from 'styled-components'
 
 export const FormDiv = styled.div`
-    // display: flex;
-    // flex-direction: column;
-    // justify-content: center;
-    // align-items: center;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
     form{
         display: flex;
         flex-direction: column;
