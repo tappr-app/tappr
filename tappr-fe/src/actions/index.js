7e6134db86081203d06f523fd1c8458490dd8d06
Landon Turner <lturner@LTurner-MBP.lan1>
2020-03-31 09:08:22 -0400

    adds reg form actions

--- tappr-fe/src/actions/index.js
+++ tappr-fe/src/actions/index.js
@@ -1,6 +1,14 @@
 import { axiosWithAuth } from "../utils/axiosWithAuth";
 
+// General Actions
+export const SET_ERROR = 'SET_ERROR'
+
+// Register Actions
 export const POST_USER = 'POST_USER'
+export const USER_CREATED = 'USER_CREATED'
+
+// Login Actions
+export const USER_LOGIN = 'USER_LOGIN'
 
 
 export const handlePostData = payload => dispatch => {
@@ -8,10 +16,14 @@ export const handlePostData = payload => dispatch => {
     axiosWithAuth()
     .post('/auth/register', payload)
     .then(res=>{
-        console.log('POST', res)
+        console.log('POST', res);
+        dispatch({ type: USER_CREATED})
     })
-    .catch(err=> console.log(console.log(err)
-    ))
+    .catch(err=>{
+        console.log(err)
+        dispatch({ type: SET_ERROR, payload: 'Party foul! Action wasn\'t completed' })
+    } 
+    )
 
 }
 
--- tappr-fe/src/reducers/beerReducer.js
+++ tappr-fe/src/reducers/beerReducer.js
@@ -1,11 +1,29 @@
+import { POST_USER, USER_CREATED, SET_ERROR } from "../actions";
+
 const initialState = {
     beer: [],
     isFetching: false,
+    isPosting: false,
     error: ''
 };
 
 export const beerReducer = (state = initialState, action) => {
   switch(action.type) {
+    case POST_USER:
+      return {
+        ...state,
+        isPosting: true
+      }
+    case USER_CREATED:
+      return {
+        ...state,
+        isPosting: false
+      }
+    case SET_ERROR:
+      return{
+        ...state,
+        error: action.payload
+      }
     default:
       return state
   };
