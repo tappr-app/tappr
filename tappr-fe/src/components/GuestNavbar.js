7ff0a9c0801205fee76838074eed9dccc8792396
Amanda Hogan <ahogan@teamworks.com>
2020-04-01 17:09:49 -0400

    Style user dashboard

--- tappr-fe/src/components/GuestNavbar.js
+++ tappr-fe/src/components/GuestNavbar.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 import { Navbar, NavLinks } from '../styles/Styled';
 
 const GuestNavbar = () => {
--- tappr-fe/src/components/UserNavbar.js
+++ tappr-fe/src/components/UserNavbar.js
@@ -1,20 +1,11 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 import { useHistory } from 'react-router-dom';
-import { Navbar } from '../styles/Styled';
+import { Navbar, NavLinks } from '../styles/Styled';
 
 const UserNavbar = (props) => {
   const history = useHistory()
   const user_username = window.localStorage.getItem('user_username');
   const user_id = window.localStorage.getItem('user_id');
-
-  const userProfile = (id) => {
-    history.push(`/my-profile/${id}`);
-  };
-
-  const userBrews = (id) => {
-    history.push(`/my-brews/${id}`);
-  };
 
   const logout = () => {
     window.localStorage.removeItem('token');
@@ -26,14 +17,11 @@ const UserNavbar = (props) => {
   return (
     <div>
       <Navbar>
-        <Link to="/my-dashboard">Tap List</Link>
-        <Link to={`/my-brews/${user_id}`}>My Favorite Brews</Link>
-        {/* <button onClick={() => userBrews(user_id)}>My Favorite Brews</button> */}
-        <Link to="/add-a-beer">Add a Beer</Link>
-        <Link to='/login' onClick={() => logout()}>My Favorite Brews</Link>
-        {/* <button onClick={() => logout()}>Logout</button> */}
-        <Link to={`/my-profile/${user_id}`}>{user_username}</Link>
-        {/* <button onClick={() => userProfile(user_id)}>{user_username}</button> */}
+        <NavLinks href='/my-dashboard'>Tap List</NavLinks>
+        <NavLinks href={`/my-brews/${user_id}`}>My Favorite Brews</NavLinks>
+        <NavLinks href="/add-a-beer">Add a Beer</NavLinks>
+        <NavLinks href='/login' onClick={() => logout()}>Logout</NavLinks>
+        <NavLinks href={`/my-profile/${user_id}`}>{user_username}</NavLinks>
       </Navbar>
     </div>
   );
