636a2d6a8ab91dfe40e395241c99e30cdbb01cd9
Landon Turner <lturner@LTurner-MBP.lan1>
2020-04-02 15:46:10 -0400

    add pairing structured and ready to connect in beer details

--- tappr-fe/src/components/BeerDetails.js
+++ tappr-fe/src/components/BeerDetails.js
@@ -9,6 +9,10 @@ import { axiosWithAuth } from '../utils/axiosWithAuth';
 const BeerDetails = (props) => {
   const params = useParams();
 
+  const [editingPairing, setEditingPairing] = useState(false)
+  const [addPairing, setAddPairing] = useState({
+    food_name: ''
+  })
   const [thisBeer, setThisBeer] = useState()
   const [beerReady, setBeerReady] = useState(false)
 
@@ -25,6 +29,24 @@ const BeerDetails = (props) => {
   const updateBeer = (id) => {
     props.history.push(`/update-a-beer/${id}`)
   };
+
+  const handleEditPairing = e =>{
+    e.preventDefault();
+    setEditingPairing(true)
+  }
+  const handleChangesPairing = e =>{
+    setAddPairing({
+        ...addPairing,
+        [e.target.name]: e.target.value
+    })
+};
+console.log(addPairing)
+// const handleChangesComments = e =>{
+//   setAddPairing({
+//       ...addPairing,
+//       [e.target.name]: e.target.value
+//   })
+// };
 
   console.log('beer details for',thisBeer);
   
@@ -46,10 +68,19 @@ const BeerDetails = (props) => {
           })}</p>}
           {thisBeer.comments !== [] ? <p>Comments: No Comments. Add some below!</p>
           :
-          <p>Pairings:{thisBeer.comments.forEach(element => {
+          <p>Comments:{thisBeer.comments.forEach(element => {
             return <li>{element}</li>
-          })}</p>}          
-        </div>
+          })}</p>}  
+          {editingPairing ? 
+          (<form>
+            <label>Food name:</label>
+            <input name='food_name' onChange={handleChangesPairing}/>
+            <button>Post</button>
+            <span onClick={()=> setEditingPairing(false)}>Cancel</span>
+          </form>)
+          : (<button onClick={handleEditPairing}>Add Pairing</button>)
+          }        
+        </div>  
       : <div>Loading brewski</div>
       }
     </div>
