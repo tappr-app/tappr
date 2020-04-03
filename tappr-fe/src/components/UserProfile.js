2df80e207d5c759c61780daa9cf35ea105fde7a4
Amanda Hogan <ahogan@teamworks.com>
2020-04-03 12:10:28 -0400

    Setup user profile layout

--- tappr-fe/src/components/UserProfile.js
+++ tappr-fe/src/components/UserProfile.js
@@ -14,13 +14,32 @@ const UserProfile = props => {
       <UserNavbar />
       {props.readyToMount ? 
         <div>
-        <span></span>
-        <h2>{props.active_user.user.username}</h2>
-        <h3>Bio</h3>
-        <p>{props.active_user.user.bio}</p>
-        <div>
-          <h3>Badges</h3>
-        </div>
+          <span></span>
+          <h2>{props.active_user.user.username}</h2>
+          <div>
+            {props.active_user.user.user_image !== null ?
+              <div> 
+                <img src={props.active_user.user.user_image} alt={props.active_user.user.username} />
+                <button>Edit Profile Image</button>
+              </div> : <button>Add Profile Image</button>}
+          </div>
+          <div>
+            <p>{props.active_user.user.username}</p>
+            <button>Edit</button>
+          </div>
+          <button>Change Password</button>
+          <div>
+            <p>{props.active_user.user.age}</p>
+            <button>Edit</button>
+          </div>
+          <h3>Bio</h3>
+          <div>
+            <p>{props.active_user.user.bio}</p>
+            <button>Edit</button>
+          </div>
+          {/* <div>
+            <h3>Beer Badges</h3>
+          </div> */}
         </div>
       :
       <div>Loading your profile...</div>
